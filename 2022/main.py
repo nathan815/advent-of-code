2ff181ec23e5449f5e9fd326e75c3e844b604f16
Nathan Johnson <nathancj224@gmail.com>
2022-12-08 18:46:30 -0500

    remove solution_ prefix from py files

--- 2022/main.py
+++ 2022/main.py
@@ -1,10 +1,10 @@
 from day1.solution import run_day1
-from day2.solution_part1 import run_day2_part1
-from day2.solution_part2 import run_day2_part2
-from day3.solution_part1 import run_day3_part1
-from day3.solution_part2 import run_day3_part2
-from day4.solution_part1 import run_day4_part1
-from day4.solution_part2 import run_day4_part2
+from day2.part1 import run_day2_part1
+from day2.part2 import run_day2_part2
+from day3.part1 import run_day3_part1
+from day3.part2 import run_day3_part2
+from day4.part1 import run_day4_part1
+from day4.part2 import run_day4_part2
 from day5.part1 import run_day5_part1
 from day5.part2 import run_day5_part2
 
