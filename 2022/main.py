d74ea7d3dd0099ae8c6f715c5062a94c2353c0a6
Nathan Johnson <nathancj224@gmail.com>
2022-12-09 00:18:20 -0500

    add day7 to main.py

--- 2022/main.py
+++ 2022/main.py
@@ -9,6 +9,8 @@ from day5.part1 import run_day5_part1
 from day5.part2 import run_day5_part2
 from day6.part1 import run_day6_part1
 from day6.part2 import run_day6_part2
+from day7.part1 import run_day7_part1
+from day7.part2 import run_day7_part2
 
 run_day1()
 run_day2_part1()
@@ -21,3 +23,5 @@ run_day5_part1()
 run_day5_part2()
 run_day6_part1()
 run_day6_part2()
+run_day7_part1()
+run_day7_part2()
