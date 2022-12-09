df137dd9d30b74671578961d55517455a6253d59
Nathan Johnson <nathancj224@gmail.com>
2022-12-08 20:03:12 -0500

    make main work for day6 and fix pytest error

--- 2022/main.py
+++ 2022/main.py
@@ -7,6 +7,8 @@ from day4.part1 import run_day4_part1
 from day4.part2 import run_day4_part2
 from day5.part1 import run_day5_part1
 from day5.part2 import run_day5_part2
+from day6.part1 import run_day6_part1
+from day6.part2 import run_day6_part2
 
 run_day1()
 run_day2_part1()
@@ -17,3 +19,5 @@ run_day4_part1()
 run_day4_part2()
 run_day5_part1()
 run_day5_part2()
+run_day6_part1()
+run_day6_part2()
