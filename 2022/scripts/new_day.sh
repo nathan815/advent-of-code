f3e7dc59395a8ad88147ca99c3176fbb7b0b0036
Nathan Johnson <nathancj224@gmail.com>
2022-12-13 21:20:47 -0500

    add __init__ files to all days to allow pytest to work from top level

--- 2022/scripts/new_day.sh
+++ 2022/scripts/new_day.sh
@@ -7,6 +7,7 @@ DIRECTORY=day$DAY
 if [ ! -d "$DIRECTORY" ]; then
     mkdir $DIRECTORY
     cd $DIRECTORY
+    touch __init__.py
     touch {test_,}part{1,2}.py
     touch problem.txt
     touch input.txt
