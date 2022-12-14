e2986de9e19769f2558f0d2b231160d96b46dfbd
Nathan Johnson <nathancj224@gmail.com>
2022-12-13 21:04:36 -0500

    Upgrade main.py to sort day directories correctly by int value

--- 2022/main.py
+++ 2022/main.py
@@ -1,16 +1,33 @@
-import os
+from datetime import datetime
 import importlib
+from pathlib import Path
 
-# Find and run all solution files.
+# Find and run all solution files
 
-SOLUTION_FILE_NAMES = ['part1.py', 'part2.py', 'solution.py']
+SOLUTION_FILE_NAMES = ["part1.py", "part2.py", "solution.py"]
 
-for dirpath, dirname, filenames in os.walk('.'):
-    for fname in filenames:
-        if fname in SOLUTION_FILE_NAMES:
-            file = dirpath + '/' + fname
-            print(f'--- Running {file} ---')
-            modname = file.replace('/', '.').replace('.py', '').lstrip('.')
+# Filter and then sort day directories correctly (by int value)
+sorted_paths = sorted(
+    filter(
+        lambda p: p.name.startswith("day"),
+        Path(".").iterdir(),
+    ),
+    key=lambda p: int(p.name.replace("day", "")),
+)
+
+for path in sorted_paths:
+    for file in path.iterdir():
+        if file.name in SOLUTION_FILE_NAMES:
+            filepath = str(file)
+            print(f"--- Running {filepath} ---")
+            modname = filepath.replace("/", ".").replace(".py", "").lstrip(".")
             mod = importlib.import_module(modname)
-            mod.run()
+            if hasattr(mod, "run"):
+                start = datetime.now()
+                mod.run()
+                runtime = datetime.now() - start
+                print()
+                print("Runtime:", runtime)
+            else:
+                print("ERROR: Missing run() function!")
             print()
